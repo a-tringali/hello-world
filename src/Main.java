95d9630f5878bc06b7e5542d22704b004f1f7234
HenryWRausch <114879639+HenryWRausch@users.noreply.github.com>
2022-10-04 15:41:50 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Andrew Tringali: Hello world!");
+        System.out.println("Henry Rausch: Hello world!");
     }
 }
